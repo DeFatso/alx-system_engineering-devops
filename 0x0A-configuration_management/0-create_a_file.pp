b0e91a0508607397892232b6471d04e0bbf76294
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-01-19 19:49:58 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-/*Using Puppet, create a file*/
-file  { '/tmp/school':
+#Using Puppet, create a file
+file  { 'school':
   ensure  => file,
+  path    => '/tmp/shcool',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
