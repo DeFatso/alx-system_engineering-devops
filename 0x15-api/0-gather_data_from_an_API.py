ca490d6b1ad9bb3959edc0eed7b551118c14f6e5
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-02-19 17:42:36 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,14 +8,15 @@ import requests
 import sys
 from sys import argv
 
+if __name__ == "__main__":
+    """ base link """
+    link = "https://jsonplaceholder.typicode.com/"
+    employee_id = argv[1]
+    user = requests.get(link + "users/{}".format(employee_id)).json()
+    parameters = {"userId": employee_id}
+    todos = requests.get(link + "todos", parameters).json()
 
-link = "https://jsonplaceholder.typicode.com/"
-employee_id = argv[1]
-user = requests.get(link + "users/{}".format(employee_id)).json()
-parameters = {"userId": employee_id}
-todos = requests.get(link + "todos", parameters).json()
-
-task_completed = [t["title"] for t in todos if t.get("completed")]
-print("Employee {} is done with tasks({}/{}):".format(
-    user.get("name"), len(task_completed), len(todos)))
-[print("\t {}".format(complete)) for complete in task_completed]
+    task_completed = [t["title"] for t in todos if t.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(task_completed), len(todos)))
+    [print("\t {}".format(complete)) for complete in task_completed]
