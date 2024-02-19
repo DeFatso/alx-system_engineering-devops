55b490e42fdd093d223a632d7921d3a8a8646869
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-02-19 17:38:34 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    returns information about his/her TODO list progress.
+"""
+
+
+import requests
+import sys
+from sys import argv
+
+
+link = "https://jsonplaceholder.typicode.com/"
+employee_id = argv[1]
+user = requests.get(link + "users/{}".format(employee_id)).json()
+parameters = {"userId": employee_id}
+todos = requests.get(link + "todos", parameters).json()
+
+task_completed = [t["title"] for t in todos if t.get("completed")]
+print("Employee {} is done with tasks({}/{}):".format(
+    user.get("name"), len(task_completed), len(todos)))
+[print("\t {}".format(complete)) for complete in task_completed]
