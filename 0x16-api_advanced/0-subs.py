a65205e3d6998d04e6914029c77580f12ce975a3
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-03-05 22:26:24 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    0-subs.py
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "DeFatso/App"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if response.status_code == 200:
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
